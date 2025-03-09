f0cb0d0c10d6a9e65b6e5bce94a61ed476b7db44
Lawri Darbyshire <166262530+Darbyshire64@users.noreply.github.com>
2025-03-09 16:29:00 +0000

    Fixed API.html and Fixed Self Test Issue

--- app.py
+++ app.py
@@ -54,7 +54,6 @@ try:
         ("Asda", getdata.GetData.Asda),
         ("BP", getdata.GetData.BP),
         ("Esso Tesco", getdata.GetData.ET),
-        ("Tesco", getdata.GetData.Tesco),
         ("Jet", getdata.GetData.Jet),
         ("Karen", getdata.GetData.Karen),
         ("Morrisons", getdata.GetData.Morisons),
